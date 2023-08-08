267c432e402a47ba83ee2ac7e656d038a9cf9faf
VenkataBhaskar <111889155+VenkataBhaskarr@users.noreply.github.com>
2023-08-08 14:12:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(CORS())
 app.use(express.json())
 
 app.get("/" , (req,res) => {
-  res.send("This is updated for the fifth time")
+  res.send("This is updated for the sixth time")
 })
 
 app.listen(3000, () => {
